9eee19daf993b48d0357fefa07a902a0dd1e608f
ediazgarcia <eddymanueldiazgarcia@gmail.com>
2021-12-07 00:08:44 -0400

    add conection a mongodb

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,29 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./class/server"));
 const user_1 = __importDefault(require("./routes/user"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const server = new server_1.default();
 // Ruta de la app
 server.app.use('/user', user_1.default);
+//Conectar DB
+mongoose_1.default.connect('mongodb://localhost:27017/petsdb', (err) => __awaiter(void 0, void 0, void 0, function* () {
+    if (err)
+        throw err;
+    console.log("conncted to db");
+}));
 // Levantar express
 server.start(() => {
     console.log(`Servidor corriendo en port ${server.port}`);
