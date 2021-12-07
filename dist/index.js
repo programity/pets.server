f99163ab3c5e66fbb20ec221de824f61141b832a
ediazgarcia <eddymanueldiazgarcia@gmail.com>
2021-12-06 23:23:51 -0400

    Add services REST

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./class/server"));
+const user_1 = __importDefault(require("./routes/user"));
 const server = new server_1.default();
+// Ruta de la app
+server.app.use('/user', user_1.default);
 // Levantar express
 server.start(() => {
     console.log(`Servidor corriendo en port ${server.port}`);
