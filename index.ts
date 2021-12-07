f64908d2444e5d48cf7c0a20632191f5326fdd2f
ediazgarcia <eddymanueldiazgarcia@gmail.com>
2021-12-06 23:23:51 -0400

    Add services REST

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import Server from "./class/server";
+import userRoutes from "./routes/user";
 
 const server = new Server();
+
+
+// Ruta de la app
+server.app.use('/user', userRoutes)
 
 
 // Levantar express
