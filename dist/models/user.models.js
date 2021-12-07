1a2efe4cc5368fbac56c73c559209406d818050e
ediazgarcia <eddymanueldiazgarcia@gmail.com>
2021-12-07 15:04:31 -0400

    create services login

--- dist/models/user.models.js
+++ dist/models/user.models.js
@@ -1,7 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Users = void 0;
 const mongoose_1 = require("mongoose");
+const bcrypt_1 = __importDefault(require("bcrypt"));
 const userSchema = new mongoose_1.Schema({
     name: {
         type: String,
@@ -19,6 +23,14 @@ const userSchema = new mongoose_1.Schema({
     password: {
         type: String,
         required: [true, 'La contrasena es requerida!']
+    }
+});
+userSchema.method('comparePassword', function (password = '') {
+    if (bcrypt_1.default.compareSync(password, this.password)) {
+        return true;
+    }
+    else {
+        return false;
     }
 });
 exports.Users = (0, mongoose_1.model)('Users', userSchema);
