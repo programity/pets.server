8d4d8e17c88f4dee22791852eff5915507962479
ediazgarcia <eddymanueldiazgarcia@gmail.com>
2021-12-07 00:08:44 -0400

    add conection a mongodb

--- index.ts
+++ index.ts
@@ -1,15 +1,23 @@
 import Server from "./class/server";
 import userRoutes from "./routes/user";
+import mongoose from "mongoose";
 
 const server = new Server();
 
-
 // Ruta de la app
-server.app.use('/user', userRoutes)
+server.app.use('/user', userRoutes);
+
+//Conectar DB
+
+mongoose.connect('mongodb://localhost:27017/petsdb', async (err) => {
+    if (err) throw err;
+    console.log("conncted to db")
+});
+
 
 
 // Levantar express
 server.start(() => {
     console.log(`Servidor corriendo en port ${server.port}`);
-    
+
 });
