29e5275dd60280b5e73f2c2887882f1d7c622c46
ediazgarcia <eddymanueldiazgarcia@gmail.com>
2021-12-07 15:04:31 -0400

    create services login

--- models/user.models.ts
+++ models/user.models.ts
@@ -1,7 +1,6 @@
 
-
-
 import { Schema, model, Document } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema({
     name: {
@@ -24,11 +23,22 @@ const userSchema = new Schema({
 
 });
 
+userSchema.method('comparePassword', function (password: string = ''): boolean {
+
+    if (bcrypt.compareSync(password, this.password)) {
+        return true;
+    } else {
+        return false;
+    }
+});
+
 interface IUser extends Document {
     name: string;
     avatar: string;
     email: string;
     password: string;
+
+    comparePassword(password: string): boolean;
 }
 
 export const Users = model<IUser>('Users', userSchema);
--- routes/user.ts
+++ routes/user.ts
@@ -4,13 +4,45 @@ import bcrypt from 'bcrypt';
 
 const userRoutes = Router();
 
+
+// Login
+userRoutes.post('/login', (req: Request, res: Response) => {
+
+    const body = req.body;
+
+    Users.findOne({ email: body.email }, (err: any, userDB: any) => {
+        if (err) throw err;
+
+        if (!userDB) {
+            return res.json({
+                ok: false,
+                mensaje: 'Usuario/contrasena no son correctos'
+            });
+        }
+
+        if ( userDB.comparePassword(body.password)) {
+            res.json({
+                ok: true,
+                token: 'AJHHJAGDHJAGJHDAGHFFHGGHH'
+            })
+        } else {
+            return res.json({
+                ok: false,
+                mensaje: 'Usuario/contrasena no son correctos ***'
+            });
+        }
+    });
+
+});
+
+// Create a user
 userRoutes.post('/create', (req: Request, res: Response) => {
 
     const users = {
         name: req.body.name,
         email: req.body.email,
         password: bcrypt.hashSync(req.body.password, 10),
-        avatar : req.body.avatar
+        avatar: req.body.avatar
     };
 
     Users.create(users)
