bb2ca4424e07a81cc7962e1c46015d417c207b20
ediazgarcia <eddymanueldiazgarcia@gmail.com>
2021-12-08 16:01:49 -0400

    add to get a posts pagination

--- dist/routes/post.js
+++ dist/routes/post.js
@@ -13,6 +13,24 @@ const express_1 = require("express");
 const authentication_1 = require("../middlewares/authentication");
 const post_models_1 = require("../models/post.models");
 const postRoutes = (0, express_1.Router)();
+// getPaginationPost
+postRoutes.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    let pagina = Number(req.query.pagina) || 1;
+    let skip = pagina - 1;
+    skip = skip * 10;
+    const posts = yield post_models_1.Post.find()
+        .sort({ _id: -1 })
+        .skip(skip)
+        .limit(10)
+        .populate('user', '-password')
+        .exec();
+    res.json({
+        ok: true,
+        pagina,
+        posts
+    });
+}));
+// Create POST
 postRoutes.post('/', [authentication_1.verifyToken], (req, res) => {
     const body = req.body;
     body.user = req.user._id;
