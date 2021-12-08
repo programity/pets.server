bc0245ffbe9e52713bf3acf659fe31ebbd058fbe
ediazgarcia <eddymanueldiazgarcia@gmail.com>
2021-12-08 16:01:49 -0400

    add to get a posts pagination

--- routes/post.ts
+++ routes/post.ts
@@ -6,7 +6,30 @@ import { Post } from '../models/post.models';
 
 const postRoutes = Router();
 
+// getPaginationPost
+postRoutes.get('/', async (req: any, res: Response) => {
 
+    let pagina =  Number(req.query.pagina) || 1;
+    let skip = pagina - 1;
+    skip = skip * 10;
+
+
+    const posts = await Post.find()
+        .sort({ _id: - 1 })
+        .skip(skip)
+        .limit(10)
+        .populate('user', '-password')
+        .exec();
+
+    res.json({
+        ok: true,
+        pagina,
+        posts
+    });
+});
+
+
+// Create POST
 postRoutes.post('/', [verifyToken], (req: any, res: Response) => {
 
     const body = req.body;
@@ -24,7 +47,6 @@ postRoutes.post('/', [verifyToken], (req: any, res: Response) => {
         res.json(err)
     });
 });
-
 
 
 export default postRoutes;
